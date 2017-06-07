35b1b460446693656c1320e1fa44691ae474efb3
Anik Yadav <anik.yadav@bison.howard.edu>
2017-06-07 00:03:28 -0700

    part 2b

---
+++ p2.py
@@ -0,0 +1,96 @@
+from PIL import Image
+from numpy import array
+import matplotlib.pyplot as plt
+
+img = array(Image.open("corrupted.png"), dtype=int)[:,:,0]
+Known = (img > 0).astype(int)
+
+naive_img = array(img)
+
+for i in xrange(203):
+  for j in xrange(143):
+    if naive_img[i][j] == 0:
+      summ = 0
+      num_neigh = 0
+
+      # update spots with avg(known neighbors)
+      if (i==0 and j==0):
+        summ = (naive_img[i][j+1] + naive_img[i+1][j])
+        if naive_img[i][j+1] > 0:
+          num_neigh+=1
+        if naive_img[i+1][j] > 0:
+          num_neigh+=1
+      elif (i==0 and j==142):
+        summ = (naive_img[i][j-1] + naive_img[i+1][j])
+        if naive_img[i][j-1] > 0:
+          num_neigh+=1
+        if naive_img[i+1][j] > 0:
+          num_neigh+=1
+      elif (i==202 and j==0):
+        summ = (naive_img[i][j+1] + naive_img[i-1][j])
+        if naive_img[i][j+1] > 0:
+          num_neigh+=1
+        if naive_img[i-1][j] > 0:
+          num_neigh+=1
+      elif (i==202 and j==142):
+        summ = (naive_img[i][j-1] + naive_img[i-1][j])
+        if naive_img[i][j-1] > 0:
+          num_neigh+=1
+        if naive_img[i-1][j] > 0:
+          num_neigh+=1
+
+      elif (i>0 and i<202 and j>0 and j<142):
+        summ = (naive_img[i][j-1] + naive_img[i][j+1] + naive_img[i-1][j] + naive_img[i+1][j])
+        if naive_img[i][j-1] > 0:
+          num_neigh+=1
+        if naive_img[i][j+1] > 0:
+          num_neigh+=1
+        if naive_img[i-1][j] > 0:
+          num_neigh+=1
+        if naive_img[i+1][j] > 0:
+          num_neigh+=1
+
+      else:
+        if (i==0 and j<142 and j>0):
+          summ = (naive_img[i][j-1] + naive_img[i][j+1] + naive_img[i+1][j])
+          if naive_img[i][j-1] > 0:
+            num_neigh+=1
+          if naive_img[i][j+1] > 0:
+            num_neigh+=1
+          if naive_img[i+1][j] > 0:
+            num_neigh+=1
+          
+        if (j==0 and i<202 and i>0):
+          summ = (naive_img[i][j+1] + naive_img[i-1][j] + naive_img[i+1][j])
+          if naive_img[i][j+1] > 0:
+            num_neigh+=1
+          if naive_img[i-1][j] > 0:
+            num_neigh+=1
+          if naive_img[i+1][j] > 0:
+            num_neigh+=1
+          
+        if (i==202 and j<142 and j>0):
+          summ = (naive_img[i][j-1] + naive_img[i][j+1] + naive_img[i-1][j])
+          if naive_img[i][j-1] > 0:
+            num_neigh+=1
+          if naive_img[i][j+1] > 0:
+            num_neigh+=1
+          if naive_img[i-1][j] > 0:
+            num_neigh+=1
+          
+        if (j==142 and i<202 and i>0):
+          summ = (naive_img[i][j-1] + naive_img[i-1][j] + naive_img[i+1][j])
+          if naive_img[i][j-1] > 0:
+            num_neigh+=1
+          if naive_img[i-1][j] > 0:
+            num_neigh+=1
+          if naive_img[i+1][j] > 0:
+            num_neigh+=1
+
+      if num_neigh > 0:
+        naive_img[i][j] = ((summ*1.0)/num_neigh)
+
+plt.imshow(naive_img)
+plt.gray()
+plt.savefig('recovered_stanford_tree.png')  
+plt.show()
