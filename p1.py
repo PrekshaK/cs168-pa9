2e1c1d8a3050e067fd7663db6c14feb82f3b4b87
Preksha Koirala <prekshakoirala@gmail.com>
2017-06-07 01:59:09 -0700

    part 1 b done hopefully lol

--- p1.py
+++ p1.py
@@ -1,46 +1,33 @@
 import numpy as np
 from scipy import ndimage
 from PIL import Image
-import cvxpy as cvx
+from cvxpy import *
 
 mean, var = 0, 1
-A = np.random.normal(loc =0, scale =1, size=(1200, 1200))
+m = 1200
+A = np.random.normal(loc =0, scale =1, size=(m, m))
 
 text_file = open("p9_images/wonderland-tree.txt", "r")
+# 0 is black, 1 is white
 lines = text_file.readlines()
 x = []
 for line in lines:
     for i in line.strip('\n'):
         x.append(int(i))
 
-#print x[0:100]
-r = 100
-br = np.dot(A[0:r], x)
-
-
-xbar = cvx.Variable(1200)
+r = 600
 Ar = A[0:r]
-dotpr = np.dot(A[0:r], xbar)
-
-constraints = [xbar >= 0]
-for i in xrange(r):
-    # tosum = [Ar[i][j]*xbar[i] for j in xrange(1200)]
-    summation = 0
-    for j in xrange(1200):
-        summation += Ar[i][j]*xbar[i]
-
-    #print tosum
-    #summation = cvx.sum_entries(tosum)
-    #print summation
-    bri = br[i] + 10**-5
-    brii = br[i] - 10**-5
-    #constraints.append(summation >= brii)
-    constraints.append(summation <= bri)
+br = np.dot(Ar, x)
 
 
-obj = cvx.Minimize(cvx.norm(xbar, 1))
-prob = cvx.Problem(obj, constraints)
-prob.solve()
+xbar = Variable(m)
+objective = Minimize(norm(xbar, 1))
+constraints = [0 <= xbar, xbar <= 1, Ar*xbar - br == 0]
+prob = Problem(objective, constraints)
+print "Optimal value", prob.solve()
+print "Optimal var"
+print xbar.value 
 
-print "status is ", prob.status
-print xbar.value
+print "norm of xbar is ", np.linalg.norm(xbar.value, ord=1)
+print "norm of x is ", np.linalg.norm(x, ord=1)
+
