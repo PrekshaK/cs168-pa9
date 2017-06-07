d70e0b7a7e5dc12723ab41f7402da7085101e0f0
Preksha Koirala <prekshakoirala@gmail.com>
2017-06-07 03:26:58 -0700

    part1 b & c

--- p1.py
+++ p1.py
@@ -15,22 +15,22 @@ for line in lines:
     for i in line.strip('\n'):
         x.append(int(i))
 
-# r = 600
-# Ar = A[0:r]
-# br = np.dot(Ar, x)
+r = 600
+Ar = A[0:r]
+br = np.dot(Ar, x)
 
 
-# xbar = Variable(m)
-# objective = Minimize(norm(xbar, 1))
-# constraints = [0 <= xbar, xbar <= 1, Ar*xbar - br == 0]
-# prob = Problem(objective, constraints)
-# prob.solve()
-# # print "Optimal value", prob.solve()
-# # print "Optimal var"
-# print xbar.value 
+xbar = Variable(m)
+objective = Minimize(norm(xbar, 1))
+constraints = [0 <= xbar, xbar <= 1, Ar*xbar - br == 0]
+prob = Problem(objective, constraints)
+prob.solve()
+# print "Optimal value", prob.solve()
+# print "Optimal var"
+print xbar.value 
 
-# print "norm of xbar is ", np.linalg.norm(xbar.value, ord=1)
-# print "norm of x is ", np.linalg.norm(x, ord=1)
+print "norm of xbar is ", np.linalg.norm(xbar.value, ord=1)
+print "norm of x is ", np.linalg.norm(x, ord=1)
 
 
 
@@ -53,12 +53,12 @@ def findOptimal(r):
 
     diff = [math.fabs(x[i] - xbar.value[i]) for i in xrange(len(x))]
     norm_diff = np.linalg.norm(diff, ord=1)
-    print "norm diff for this candidate is ", norm_diff
+    print "norm diff ", norm_diff
     return norm_diff
 
 
 def binarySearch(rs, start, end, r):
-    if start >= end:
+    if start > end:
         print "so the least r is ", r
         return r
 
@@ -72,6 +72,8 @@ def binarySearch(rs, start, end, r):
 
     elif findOptimal(candidate) > 0.001:
         binarySearch(rs, mid+1, end, r)
+
+    return r
 
 rs = [i for i in xrange(600)]
 least_r = binarySearch(rs, 0, len(rs)-1, -1)
