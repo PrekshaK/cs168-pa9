552c3d8cb5304e6a6f9d251459e40f2bfe4bd20f
Preksha Koirala <prekshakoirala@gmail.com>
2017-06-07 19:13:25 -0700

    part 3

--- p3.py
+++ p3.py
@@ -3,8 +3,8 @@ from scipy import ndimage
 from cvxpy import *
 import math
 
-m = 25
-n = 5
+m = 5
+n = 3
 R = np.random.normal(loc =0, scale =1, size=(m, n))
 M = R.dot(R.T)
 
@@ -14,11 +14,16 @@ for i in xrange(m):
     for j in xrange(n):
         M_hat[i][j] = np.random.choice([0, M[i][j]], p=[P, 1-P])
 
+
+# According to question
+
 # Now recover M from M_hat using cvx
 # U = variable
+
 # Minimize rank(U)
 # Constraint that U matches the known entries of M_hat.
 
+#           can be restates as:
 # AU = eigenvalues of U
 # Minimize L1 norm of AU
 # Constraints are:
@@ -26,6 +31,7 @@ for i in xrange(m):
 # U matches the known entries of M_hat
 # AU is non-negative
 
+#           can be restated as
 # Minimize sum of diagonal entries of U
 # Constraints are:
 # U is symmetric
@@ -33,18 +39,22 @@ for i in xrange(m):
 # U matches the known entries of M_hat
 
 
-U = Variable(m, m)                     # Semi definite positive matrix of dim m,m
-objective = Minimize(lambda_sum_largest(U, m))
+masked = np.ma.masked_where(M_hat==0, M_hat)
+print "norm of M_hat is ", np.linalg.norm(M_hat, ord=1)
 
-# Constraint that U matches the known entries of M_hat is confusing
-# Need to do masking first
-constraints = [U == M_hat, U >> 0]
+U = Variable(m, m) 
+objective = Minimize(trace(U))
+
+# Constraint that U matches the known entries of M_hat is confusing    
+constraints = []
+constraints.append(U>>0)
+for i in xrange(m):
+    for j in xrange(len(masked.mask[i])):
+        if masked.mask[i][j] == False:
+            constraints.append(U[i, j] == masked[i][j])
 
 prob = Problem(objective, constraints)
+print "the prob status is ", prob.status
 prob.solve()
-print U.value 
-
-# returns None for now
-
-
+print np.linalg.norm(U.value, ord=1)
 
